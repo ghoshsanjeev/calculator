81924606d3485a85482b04308a68899eb344c31b
Sanjeev <sanjeev.ghosh@trellix.com>
2023-12-29 02:12:32 +0530

    Style, font and functionalities added

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Project show cases a calculator developed using Html and JS and little bit of CS
 
 ### Demo
 [Standard Calculator](https://ghoshsanjeev.github.io/calculator/standard-calculator.html)
+
+
+### Credits
+[Digital-7 font](https://www.1001fonts.com/digital+clock-fonts.html)
--- calculator.js
+++ calculator.js
@@ -1,23 +1,26 @@
 var arg1 = "", arg2 = "";
-isOperatorSet = false;
+let isArg1Set = false;
 var operator = ""
 
 function reset() {
     arg1 = "", arg2 = "";
-    isOperatorSet = false;
+    isArg1Set = false;
     operator = ""
+    showValue("");
 }
 
 function setArg1(txt) {
-    arg1 = arg1.concat(txt)
+    arg1 = arg1.concat(txt);
+    showValue(arg1);
 }
 
 function setArg2(txt) {
-    arg2 = arg2.concat(txt)
+    arg2 = arg2.concat(txt);
+    showValue(arg2);
 }
 
 function setArg(txt) {
-    if (isOperatorSet) {
+    if (isArg1Set) {
         setArg2(txt);
     } else {
         setArg1(txt);
@@ -26,7 +29,7 @@ function setArg(txt) {
 
 function setOperator(op) {
     operator = op;
-    isOperatorSet = true;
+    isArg1Set = true;
 }
 
 function compute(operator, num1, num2) {
@@ -53,8 +56,32 @@ function compute(operator, num1, num2) {
     return value;
 }
 
+function showValue(value) {
+    document.getElementById("result").value = value;
+    updateResult();
+}
+
 function calculate() {
     let result = compute(operator, Number(arg1), Number(arg2));
-    document.getElementById("result").value = result;
+    console.log("result: ", result);
+    reset()
     setArg1(result);
+}
+
+function updateResult() {
+    let resultElement = document.getElementById("result");
+    let originalValue = resultElement.value;
+    let resultValue = parseFloat(resultElement.value);
+    if (!isNaN(resultValue)) {
+        let formattedResult = new Intl.NumberFormat().format(resultValue);
+        resultElement.value = formattedResult;
+    } else {
+        resultElement.value = originalValue;
+    }
+}
+
+function deleteFromRight() {
+    let valueWithoutCommas = document.getElementById("result").value.replaceAll(',', '');
+    console.log("valueWithoutCommas", valueWithoutCommas)
+    showValue(valueWithoutCommas.slice(0, -1));
 }
